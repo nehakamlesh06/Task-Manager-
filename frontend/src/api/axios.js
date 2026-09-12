edc9d3551c94e8d082dbf8967e90df1d3de51ba7
Neha Kamlesh <nehakamlesh03@gmail.com>
2026-09-12 16:55:10 +0530

    Point frontend to live Render backend

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,8 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:8080/api',
-});
+  baseURL:'https://task-manager-ujm3.onrender.com/api',});
 
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem('token');
